53de07941c9b21608926569e6583a24e70da0d34
Pablo Troncoso <pablo.metalheart@gmail.com>
2023-04-29 19:33:19 -0400

    commit nro 16

--- cride/circles/views/circles.py
+++ cride/circles/views/circles.py
@@ -13,6 +13,12 @@ from cride.circles.models import Circle
 class CircleViewSet(viewsets.ModelViewSet):
     """Circle view set."""
 
-    queryset = Circle.objects.all()
     serializer_class = CircleModelSerializer
     permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """Restrict list to public-only."""
+        queryset = Circle.objects.all()
+        if self.action == 'list':
+            return queryset.filter(is_public=True)
+        return queryset
