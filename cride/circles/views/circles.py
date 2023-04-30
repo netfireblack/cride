daf5be5fd536a7e6dede959620bdd775c48fa63b
Pablo Troncoso <pablo.metalheart@gmail.com>
2023-04-30 12:17:50 -0400

    commit nro 19

--- cride/circles/views/circles.py
+++ cride/circles/views/circles.py
@@ -13,10 +13,12 @@ from cride.circles.serializers import CircleModelSerializer
 # Models
 from cride.circles.models import Circle, Membership
 
+
 class CircleViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
     """Circle view set."""
 
     serializer_class = CircleModelSerializer
+    lookup_field = 'slug_name'
 
     def get_queryset(self):
         """Restrict list to public-only."""
--- cride/users/views/users.py
+++ cride/users/views/users.py
@@ -1,18 +1,24 @@
 """Users views."""
 
 # Django REST Framework
-from rest_framework import status
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 # Serializers
 from cride.users.serializers import UserLoginSerializer, UserModelSerializer, UsersSignUpSerializer, AccountVerificationSerializer
 
-class UserLoginAPIView(APIView):
-    """User login API view"""
 
-    def post(self, request, *args, **kwargs):
-        """Handle HTTP POST request."""
+class UserViewSet(viewsets.GenericViewSet):
+    """User view set
+    
+    Handle sign up, login and account verification.
+    """
+
+    @action(detail=False, methods=['post'])
+    def login(self, request):
+        """User sign in."""
         serializer = UserLoginSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user, token = serializer.save()
@@ -22,22 +28,18 @@ class UserLoginAPIView(APIView):
         }
         return Response(data, status=status.HTTP_201_CREATED)
     
-class UserSignUpAPIView(APIView):
-    """User sign up API view"""
-
-    def post(self, request, *args, **kwargs):
-        """Handle HTTP POST request."""
+    @action(detail=False, methods=['post'])
+    def signup(self, request):
+        """User sign up."""
         serializer = UsersSignUpSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
         data = UserModelSerializer(user).data,
         return Response(data, status=status.HTTP_201_CREATED)
     
-class AccountVerificationAPIView(APIView):
-    """Account verification API view."""
-
-    def post(self, request, *args, **kwargs):
-        """Handle HTTP POST request."""
+    @action(detail=False, methods=['post'])
+    def verify(self, request):
+        """Account verification."""
         serializer = AccountVerificationSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
